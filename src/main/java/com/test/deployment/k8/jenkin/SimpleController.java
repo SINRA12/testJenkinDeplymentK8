126d5e55328313c4f5a755ff23c535bff48a5021
Rakesh Singh <rrrbisht91@gmail.com>
2025-07-26 17:38:36 +0530

    Update SimpleController.java
    Append the server IP  in the api response .

--- src/main/java/com/test/deployment/k8/jenkin/SimpleController.java
+++ src/main/java/com/test/deployment/k8/jenkin/SimpleController.java
@@ -1,28 +1,41 @@
 package com.test.deployment.k8.jenkin;
 
 import org.springframework.web.bind.annotation.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("/api")
 public class SimpleController {
 
+    // Method to fetch server IP
+    private String getServerIp() {
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            return inetAddress.getHostAddress();  // Return the IP address
+        } catch (UnknownHostException e) {
+            return "Unknown IP";  // In case the IP cannot be fetched
+        }
+    }
+
     @GetMapping
     public String getValue() {
-        return "GET: Hard coded value but this time checking the systemd configuration and application is inside a K8 cluster";
+        return "GET: Hard coded value but this time checking the systemd configuration and application is inside a K8 cluster. " +
+               "Server IP: " + getServerIp();
     }
 
     @PostMapping
     public String postValue() {
-        return "POST: Hard coded value";
+        return "POST: Hard coded value. Server IP: " + getServerIp();
     }
 
     @PutMapping
     public String putValue() {
-        return "PUT: Hard coded value";
+        return "PUT: Hard coded value. Server IP: " + getServerIp();
     }
 
     @DeleteMapping
     public String deleteValue() {
-        return "DELETE: Hard coded value";
+        return "DELETE: Hard coded value. Server IP: " + getServerIp();
     }
 }
